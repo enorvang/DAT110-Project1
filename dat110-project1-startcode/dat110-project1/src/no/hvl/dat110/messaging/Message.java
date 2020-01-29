95d0ed37d4805146c195f9900be532571c750199
enorvang <espen.norvang@gmail.com>
2020-01-29 10:58:25 +0100

    initial commit

---
+++ dat110-project1-startcode/dat110-project1/src/no/hvl/dat110/messaging/Message.java
@@ -0,0 +1,52 @@
+package no.hvl.dat110.messaging;
+
+
+import no.hvl.dat110.TODO;
+
+public class Message {
+	private final int MESSAGE_SIZE = 128;
+	private byte[] payload;
+
+	public Message(byte[] payload) {
+		if (payload.length <= 127) {
+			this.payload = payload;
+		}else{
+			throw new IllegalArgumentException("Payload size is too large.");
+		}
+	}
+
+	public Message() {
+		super();
+	}
+
+	public byte[] getData() {
+		return this.payload; 
+	}
+
+	public byte[] encapsulate() {
+		
+		byte[] encoded = null;
+		encoded = new byte[MESSAGE_SIZE];
+		encoded[0] = (byte) payload.length;
+		for(int i = 0; i < payload.length; i++){
+			encoded[i+1] = payload[i];
+		}
+
+
+
+
+		return encoded;
+		
+	}
+
+	public void decapsulate(byte[] received) {
+
+		int length = received[0];
+		payload = new byte[length];
+		for(int i = 0; i < length; i++){
+			payload[i] = received[i+1];
+		}
+
+		
+	}
+}
