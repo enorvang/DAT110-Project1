9c18721e5917ea89da27ab5e363f1b129421c653
enorvang <espen.norvang@gmail.com>
2020-02-04 21:59:56 +0100

    endret port og ip

--- dat110-project1-startcode/dat110-project1/src/no/hvl/dat110/system/controller/Common.java
+++ dat110-project1-startcode/dat110-project1/src/no/hvl/dat110/system/controller/Common.java
@@ -3,9 +3,9 @@ package no.hvl.dat110.system.controller;
 public class Common {
 
 	public static int DISPLAYPORT = 8080;
-	public static String DISPLAYHOST = "80.213.0.93";
+	public static String DISPLAYHOST = "localhost";
 	
 	public static int SENSORPORT = 8081;
-	public static String SENSORHOST = "80.213.0.93";
+	public static String SENSORHOST = "localhost";
 	
 }
